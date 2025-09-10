a0d5b24e9c8095c1ed5f8eebbe9bbb37450b506c
Guilherme Araujo Carneiro da Silva <guilhermearaujocarneiro26@gmail.com>
2025-09-10 14:08:41 -0300

    Update Classificador_de_faixa_eteria.java

--- Atividades com case/Classificador_de_faixa_eteria.java
+++ Atividades com case/Classificador_de_faixa_eteria.java
@@ -1,4 +1,3 @@
-
 package com.mycompany.classificador_de_faixa_eteria;
 
 import java.util.Scanner;
@@ -7,8 +6,6 @@ public class Classificador_de_faixa_eteria {
 
     public static void main(String[] args) {
 
-        double preco = 0;
-        String tipo = "";
         Scanner scanner = new Scanner(System.in);
         System.out.println("Escreva a sua idade aqui:   ");
         
@@ -18,18 +15,17 @@ public class Classificador_de_faixa_eteria {
 
                 System.out.println("Voce é uma criança");
                 break;
-                case 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23 24 25 26 27 28 29 30, :
+                case 13, 14, 15, 16, 17:
                 System.out.println("voce é um adolescente");
                 break;
-                case 18 - 59:
+                case 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51, 52, 53, 54, 55, 56, 57, 58, 59:
                 System.out.println("Voce é um adulto");
-                break;
-                case 60 - 999999999:
-                System.out.println("Voce é umidoso");
                 break;
                 
                 default:
-                System.out.println("idade invalida");
+                if (idade >= 60 && idade <= 999999){
+                System.out.println("Você é um idoso");
+                } else {System.out.println("Numero invalido!");}
         }
         
     }
