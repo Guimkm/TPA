85dbafdc4050e6f6fa54d3b3297acf59cda8ce5a
Guilherme Araujo Carneiro da Silva <guilhermearaujocarneiro26@gmail.com>
2025-09-17 22:06:35 -0300

    Update calculadora_de_numeros_pares.java

--- Atividades com Do While/calculadora_de_numeros_pares.java
+++ Atividades com Do While/calculadora_de_numeros_pares.java
@@ -5,11 +5,12 @@ public class calculadora_de_numeros_pares {
     public static void main(String[] args) { 
         
         System.out.print("Numeros pares entre 1 e 20:  ");
-        int par = 2;
+        int par = 0;
                 do {
-            System.out.println(par);
-            par += 2;
-                } while(par <= 18);
+                    par += 2;
+            System.out.println(par + " " );
+            
+                } while(par <= 20);
         
         
     }
