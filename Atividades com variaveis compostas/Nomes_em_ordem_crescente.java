2b071cebade05a1e8728ed39f41badc0aa915c4b
Guilherme Araujo Carneiro da Silva <guilhermearaujocarneiro26@gmail.com>
2025-10-09 21:29:35 -0300

    Update Nomes_em_ordem_crescente.java

--- Atividades com variaveis compostas/Nomes_em_ordem_crescente.java
+++ Atividades com variaveis compostas/Nomes_em_ordem_crescente.java
@@ -1,13 +1,20 @@
-    import java.util.Arrays;
-public class Nomes_em_ordem_crescente {
+import java.util.Arrays;
+import java.util.Collections;
+public class Nomes_ordenados {
     public static void main(String[] args) {
-        String[] nomes = {"Henrique", "Aquino rego", "Paulo bidinoso", "Luan", "Descktop Soares"};
+        String[] nomes = {"Henrique", "Aquino", "Paulo", "Luan", "Descktop Soares"};
         Arrays.sort(nomes);
         System.out.println("Nomes em ordem crescente:  ");
         for (String nome : nomes) {
             System.out.println(nome);
 
-            
+        }
+        System.out.println();
+
+        Arrays.sort(nomes, Collections.reverseOrder());
+        System.out.println("Nomes em ordem decrescente:  ");
+        for(String nome : nomes) {
+            System.out.println(nome);
         }
 
 
