4fccb8728e70c9abbac4d7aef48ab346288d6359
Guilherme Araujo Carneiro da Silva <guilhermearaujocarneiro26@gmail.com>
2025-11-22 13:29:43 -0300

    Update Matrizes_pares.java

--- Atividades com variaveis compostas/Atividade do Teams/2º Atividade do teams/Matrizes_pares/Matrizes_pares.java
+++ Atividades com variaveis compostas/Atividade do Teams/2º Atividade do teams/Matrizes_pares/Matrizes_pares.java
@@ -1,20 +1,9 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
 package com.mycompany.matrizes_pares;
 
 import java.util.Scanner;
 
-/**
- *
- * @author guilh
- */
 public class Matrizes_pares {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
@@ -60,3 +49,4 @@ scanner.close();
     }
     
 }
+
